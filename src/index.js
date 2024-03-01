dfda909db566f5ba19a63437cb63ab2527628b0d
ashishsahureaction <148655100+ashishsahureaction@users.noreply.github.com>
2024-03-01 16:09:26 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+ReactDOM.render(<hi></hi>,document.getElementById("root"));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
